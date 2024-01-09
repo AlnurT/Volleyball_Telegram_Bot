e90f4c3b39c41fd12fced651b210fe8f1c0a250e
Alnur <alnur.tazhbenov@mail.ru>
2024-01-09 15:05:12 +0300

    Command rules

--- src/project/handlers/basic.py
+++ src/project/handlers/basic.py
@@ -1,13 +1,19 @@
-from aiogram.filters import CommandStart
+from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 
 from src.create_bot import dp
 
 
-async def command_start(message: Message) -> None:
+async def command_start(message: Message):
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+
+
+async def command_rules(message: Message):
+    with open("group_rules.txt", "r", encoding="utf-8") as rules:
+        await message.answer(rules.read())
 
 
 def register_basic_handlers():
     dp.message.register(command_start, CommandStart())
+    dp.message.register(command_rules, Command("rules"))
---
+++ src/project/utils/commands.py
@@ -0,0 +1,11 @@
+from aiogram import Bot
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+
+async def set_command(bot: Bot):
+    commands = [
+        BotCommand(command="rules", description="Правила группы"),
+        BotCommand(command="poll", description="Создать опрос"),
+    ]
+
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
